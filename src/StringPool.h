76fd2a14c5596ebefc3747a3907efb8bf7d2e8c1
Chris Fallin <cfallin@c1f.net>
2018-01-23 01:52:24 -0500

    Bugfix to string-escape handling: don't silently elide \t; this breaks delimiter specs in input directives.

--- src/StringPool.h
+++ src/StringPool.h
@@ -94,7 +94,7 @@ public:
                     switch (str[i]) {
                         case '"': *p++ = '"'; break;
                         case '\\': *p++ = '\\'; break;
-                        default: break;
+                        default: *p++ = '\\'; *p++ = str[i]; break;
                     }
                     backslash = false;
                 } else {
