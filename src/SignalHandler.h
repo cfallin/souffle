8f5e53d585127fa5b13b276129b862e50d019acf
Chris Fallin <cfallin@c1f.net>
2018-07-21 02:10:19 -0400

    Disable progress messages (for production binaries)

--- src/SignalHandler.h
+++ src/SignalHandler.h
@@ -81,7 +81,7 @@ public:
     // set signal message
     void setMsg(const char* m) {
         msg = m;
-        std::cerr << "DATALOG PROGRESS: " << std::endl << m << std::endl;
+        //std::cerr << "DATALOG PROGRESS: " << std::endl << m << std::endl;
     }
 
     /***
